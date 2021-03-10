13ebd6c6dd7b9f6c6f424955c2207ee1c785c109
Otti <odeinhart@gmail.com>
2021-03-10 13:08:45 +0100

    reorganizing tiles if something breaks revert this

---
+++ tiles/CorpshausObjects/CorpshausObjects.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="CorpshausObjects" tilewidth="32" tileheight="32" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Beer.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Badge_Arminia.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Badge_Germania.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Badge_Hercynia.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Badge_Hubertia.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Badge_Makaria.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="Badge_Obotritia.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Badge_RenoPalatia.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Badge_WeisseKrone.png"/>
+ </tile>
+</tileset>
